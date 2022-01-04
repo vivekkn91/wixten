2668a7272901f3af435b7e7d2a47a07a358c5613
vivek kn <vivekkn91@gmail.com>
2022-01-04 19:28:23 +0530

    ansersdispl

---
+++ pages/query/Answerhooks.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import axios from "axios";
+// import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { FormControl, Button } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+
+export default function Answershooks(props) {
+  const [posts, setPosts] = useState([]);
+  const [increment, setincrement] = useState(0);
+  const [reloader, setreloder] = useState(props.reloadAnswer);
+  //const [decrement, setdecrement] = useState(0);
+  // const params = useParams();
+  // console.log(props.id);
+  // var gotid = params;
+  // gotid = gotid.catId;
+
+  // console.log(props.reloadAnswer);
+  // // const Answer = ({ reloadAnswer }) => {
+  useEffect(() => {
+    if (props.id != null) {
+      axios
+        .get("https://ask-over.herokuapp.com/answersapi/" + props.id)
+        .then((result) => {
+          console.table(result.data);
+          var somevariable = result;
+
+          setPosts((data) => {
+            return [...data, somevariable];
+          });
+
+          setPosts(result.data);
+        });
+    }
+  }, [props.id]);
+  // }, [increment]);
+  // };
+
+  // function decrementCounter(props) {
+  //   console.log("thsis", props);
+  //   axios
+  //     .post("https://ask-over.herokuapp.com/decrementer", {
+  //       //Answers: props,
+  //       Answer_id: props._id,
+  //       wrongcount: props.wrongcount,
+  //     })
+  //     .then(() => {
+  //       setincrement(increment + 1);
+  //     })
+  //     .catch((error) => {
+  //       console.log("handlesubmit error for blog ", error);
+  //     });
+  //   // setincrement(increment + 1);
+  // }
+  // function incrementCounter(props) {
+  //   console.log("thsis", props);
+  //   axios
+  //     .post("https://ask-over.herokuapp.com/increment", {
+  //       //Answers: props,
+  //       Answer_id: props._id,
+  //       correctcount: props.correctcount,
+  //     })
+  //     .then((res) => {
+  //       setincrement(increment + 1);
+  //       // setincrement([...increment,res.data])
+  //       // window.location.reload();
+  //     })
+  //     .catch((error) => {
+  //       console.log("handlesubmit error for blog ", error);
+  //     });
+  //   //setincrement(increment + 1);
+  // }
+
+  return (
+    <div>
+      {posts.map((personData, index) => {
+        return (
+          <Card key={index} className="cardmobile">
+            <Card.Body>
+              <h5>{personData.Answers}</h5>
+            </Card.Body>
+            <div className="buttontwo">
+              <Button
+                onClick={() => incrementCounter(personData)}
+                className="correct"
+                variant="primary"
+              >
+                Correct {personData.correctcount}
+              </Button>
+              <Button
+                onClick={() => decrementCounter(personData)}
+                //onClick={decrementCounter}
+                className="wrong"
+                variant="primary"
+              >
+                Wrong {personData.wrongcount}
+              </Button>
+            </div>
+          </Card>
+        );
+      })}
+    </div>
+  );
+}
---
+++ pages/query/[itmid].jsx
@@ -0,0 +1,80 @@
+import { useRouter } from "next/router";
+import axios from "axios";
+import InputGroup from "react-bootstrap/InputGroup";
+import Answershooks from "./Answerhooks";
+import React from "react";
+import { FormControl, Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
+export default function query() {
+  const [Item, setItem] = useState([]);
+  const router = useRouter();
+  var id = router.query.itmid;
+  var gotid = id;
+  console.log(id);
+
+  useEffect(() => {
+    if (id != null) {
+      axios
+        .get("https://ask-over.herokuapp.com/questone/" + gotid)
+        .then((result) => {
+          console.log(result);
+          // document.title = result.data[0].Name;
+          setItem(result.data);
+        });
+    }
+    //  console.table(this.state.items);
+  }, [id]);
+  return (
+    <>
+      <>
+        {/* {Item} */}
+        {Item.map((itm, k) => {
+          return (
+            <>
+              {/* <Helmet>
+                <meta charSet="utf-8" />
+                <title> wixten - {itm.Name} </title>
+                <meta name="description" content={itm.Name} />
+                <meta property="og:type" content="article" />
+                <link rel="canonical" href={itm.Name} />
+              </Helmet> */}
+              <div key={itm._id} className="Question-one">
+                <h2> {itm.Name}</h2>
+              </div>
+              <div className="username">
+                <span className="username2">--{itm.username}</span>
+              </div>
+            </>
+          );
+        })}
+      </>
+      {/* {this.props.hide ? ( */}
+      <div className="container search-box">
+        <InputGroup
+          className="mb-3"
+          //  onChange={this.question}
+          // value={this.state.ask}
+        >
+          <FormControl
+            placeholder="answer this question"
+            aria-label="answer this question"
+            aria-label="answer this question"
+            aria-describedby="basic-addon2"
+          />
+          <Button
+            type="submit"
+            // disabled={!this.state.ask}
+            // onClick={this.clickQuestion}
+            variant="outline-secondary"
+            id="button-addon2"
+          >
+            Answer
+          </Button>
+        </InputGroup>
+      </div>
+      {/* ) : null} */}
+      <Answershooks id={gotid} />
+    </>
+  );
+}
